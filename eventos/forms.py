b4a7c8221da29d1ae026baae11cddf067fade518
Javier <javieralbano2020@gmail.com>
2026-04-27 14:21:48 -0300

    Correcciones de formato visual (colores y alineacion)

--- eventos/forms.py
+++ eventos/forms.py
@@ -5,13 +5,29 @@ class PersonaForm(forms.ModelForm):
     class Meta:
         model = Persona
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control',}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control',}),
+            'email': forms.EmailInput(attrs={'class': 'form-control',}),
+        }
 
 class UbicacionForm(forms.ModelForm):
     class Meta:
         model = Ubicacion
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control',}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control',}),
+            'capacidad': forms.NumberInput(attrs={'class': 'form-control',}),
+        }
 
 class EventoForm(forms.ModelForm):
     class Meta:
         model = Evento
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control',}),
+            'fecha': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'ubicacion': forms.Select(attrs={'class': 'form-select'}),
+            'asistentes': forms.SelectMultiple(attrs={'class': 'form-select'}),
+        }
